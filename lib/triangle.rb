7d1f08c72d47049eb661c820226a2396b40a23ec
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-02 18:00:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -29,10 +29,14 @@ class Triangle
 
     def validate
       valid_triangle = [(side1+side2>side3), (side2+side3>side1), (side1+side3>side2)]
+      valid_triangle.each do |combo|
+        if !combo
+          valid_triangle << false
+          raise TriangleError if valid_triangle.include? (false)
 
-      [side1, side2, side3].each do |side|
-        valid_triangle << false if side <= 0
-        raise TriangleError if valid_triangle.include? (false)
+      # [side1, side2, side3].each do |side|
+      #   valid_triangle << false if side <= 0
+      #   raise TriangleError if valid_triangle.include? (false)
       end
       end
 
